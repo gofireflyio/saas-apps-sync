68ab74cc2970c3522ea7738a7ebbf6580393dc4a
Eran Bibi <eran@infralight.co>
2022-08-14 21:46:13 +0300

    change the alert subject

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "datadog_monitor" "new_monitor_for_the_demo" {
-  name               = "AAA This is for the demo"
+  name               = "AAA sync test"
   type               = "metric alert"
   message            = "Monitor triggered. Notify: @hipchat-channel"
   escalation_message = "Escalation message @pagerduty"
